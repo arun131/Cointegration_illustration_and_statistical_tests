6b12018919f60549f08a2dd1ec2d8436b577bf3e
arun131 <arun7569raj@gmail.com>
2024-02-17 09:49:19 +0000

    added not cointegrated pair generation

--- cointegration_synthetic_data.py
+++ cointegration_synthetic_data.py
@@ -22,6 +22,7 @@ def simple_method(plot = False):
       plt.plot(stock_1)
       plt.plot(stock_2)
 
+  return stock_1, stock_2
 
 def adapted_method(plot = False):
   # Method 2: Adapted from A drunk and her dog method
@@ -42,7 +43,31 @@ def adapted_method(plot = False):
       return np.array(X)+T0[0], np.array(Y)+T0[1]
 
   stock_1, stock_2 = make_cointegrated_pair(num_samples=2000, start_values= [100, 100], sigma = [0.01, 0.01], coint_factor=[0.1, 0.1])
+  
   if plot:
     # plot the prices
     plt.plot(stock_1)
     plt.plot(stock_2)
+
+  return stock_1, stock_2
+
+
+def not_cointegrated(plot = False):
+  # Not cointegrated pairs
+  n = 2000 # number of random values
+  start_value_1 = 200
+  stand_dev_1 = 2
+
+  start_value_2 = 250
+  stand_dev_2 = 2
+
+  # generate stock prices
+  # assuming geometric brownian motion  
+  price_changes_1 = stand_dev_1 * np.random.randn(n)
+  stock_1 = start_value_1 + np.cumsum(price_changes_1)
+
+  price_changes_2 = stand_dev_2 * np.random.randn(n)
+  stock_2 = start_value_2 + np.cumsum(price_changes_2)
+
+  return stock_1, stock_2
+
