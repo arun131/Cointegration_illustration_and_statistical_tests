42a559c965ba46a766b17825563d4907ade2886f
arun131 <arun7569raj@gmail.com>
2024-02-17 10:31:24 +0000

    corrected function

--- helper.py
+++ helper.py
@@ -11,11 +11,19 @@ def trim_and_split_data(data, start_date, end_date):
     return train_data, test_data
 
 def plot_pair(pair_a_prices, pair_b_prices, pair_a_name = "Asset_A", pair_b_name = "Asset_B"):
+    try:
+      index_a = pair_a_prices.index
+      index_b = pair_b_prices.index
+    except Exception as e:
+      index_a = np.arange(len(pair_a_prices))
+      index_b = np.arange(len(pair_b_prices))
+
     # plot the figure
     fig, ax1 = plt.subplots()
     ax1.set_xlabel('Date')
     ax1.set_ylabel(pair_a_name)
     ax1.plot(pair_a_prices.index, pair_a_prices.values, color = 'k')
+
 
     ax2 = ax1.twinx()  
     ax2.set_ylabel(pair_b_name)  
