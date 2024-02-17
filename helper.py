d93c864dd50577bcba945af82c7bce6d79551e92
arun131 <arun7569raj@gmail.com>
2024-02-17 10:38:43 +0000

    corrected plot pair function

--- helper.py
+++ helper.py
@@ -14,20 +14,24 @@ def plot_pair(pair_a_prices, pair_b_prices, pair_a_name = "Asset_A", pair_b_name
     try:
       index_a = pair_a_prices.index
       index_b = pair_b_prices.index
+      values_a = pair_a_prices.values
+      values_b = pair_b_prices.values
     except Exception as e:
       index_a = np.arange(len(pair_a_prices))
       index_b = np.arange(len(pair_b_prices))
+      values_a = pair_a_prices 
+      values_b = pair_b_prices 
 
     # plot the figure
     fig, ax1 = plt.subplots()
     ax1.set_xlabel('Date')
     ax1.set_ylabel(pair_a_name)
-    ax1.plot(index_a, pair_a_prices.values, color = 'k')
+    ax1.plot(index_a, values_a, color = 'k')
 
 
     ax2 = ax1.twinx()  
     ax2.set_ylabel(pair_b_name)  
-    ax2.plot(index_b, pair_b_prices.values, color = 'r')
+    ax2.plot(index_b, values_b, color = 'r')
 
     fig.tight_layout()  
     plt.show()
